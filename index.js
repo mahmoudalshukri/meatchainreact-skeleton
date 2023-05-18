2ca22e8702e37c78174dd6181a4f955924f249d2
unknown <mahmoud.h.alshukri@gmail.com>
2023-05-18 12:37:51 +0300

    run build command

---
+++ index.js
@@ -0,0 +1,28 @@
+// import {
+//   DivLoadingSkeleton,
+//   CardLoadingSkeleton,
+//   AdvisorsLoadingSkeleton,
+//   TeamMemberLoadingSkeleton,
+//   TableLoadingSkeleton,
+//   DigitalShareLoadingSkeleton,
+//   BuyDigitalShareLoadingSkeleton,
+//   VisitingScheduleLoadingSkeleton
+// } from "./src";
+// export {
+//   DivLoadingSkeleton,
+//   CardLoadingSkeleton,
+//   AdvisorsLoadingSkeleton,
+//   TeamMemberLoadingSkeleton,
+//   TableLoadingSkeleton,
+//   DigitalShareLoadingSkeleton,
+//   BuyDigitalShareLoadingSkeleton,
+//   VisitingScheduleLoadingSkeleton
+// };
+export { default as DivLoadingSkeleton } from "./src/components/DivLoadingSkeleton";
+export { default as CardLoadingSkeleton } from "./src/components/CardLoadingSkeleton";
+export { default as AdvisorsLoadingSkeleton } from "./src/components/AdvisorsLoadingSkeleton";
+export { default as TeamMemberLoadingSkeleton } from "./src/components/TeamMemberLoadingSkeleton";
+export { default as TableLoadingSkeleton } from "./src/components/TableLoadingSkeleton";
+export { default as DigitalShareLoadingSkeleton } from "./src/components/DigitalShareLoadingSkeleton";
+export { default as BuyDigitalShareLoadingSkeleton } from "./src/components/BuyDigitalShareLoadingSkeleton";
+export { default as VisitingScheduleLoadingSkeleton } from "./src/components/VisitingScheduleLoadingSkeleton";
---
+++ src/index.js
@@ -0,0 +1,8 @@
+// export { default as DivLoadingSkeleton } from "./components/DivLoadingSkeleton";
+// export { default as CardLoadingSkeleton } from "./components/CardLoadingSkeleton";
+// export { default as AdvisorsLoadingSkeleton } from "./components/AdvisorsLoadingSkeleton";
+// export { default as TeamMemberLoadingSkeleton } from "./components/TeamMemberLoadingSkeleton";
+// export { default as TableLoadingSkeleton } from "./components/TableLoadingSkeleton";
+// export { default as DigitalShareLoadingSkeleton } from "./components/DigitalShareLoadingSkeleton";
+// export { default as BuyDigitalShareLoadingSkeleton } from "./components/BuyDigitalShareLoadingSkeleton";
+// export { default as VisitingScheduleLoadingSkeleton } from "./components/VisitingScheduleLoadingSkeleton";
